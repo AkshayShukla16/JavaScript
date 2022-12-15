316d9a7d576a1a0b2463f10667a900f959a77fde
AKSHAY SHUKLA <98264215+AkshayShukla16@users.noreply.github.com>
2022-12-15 12:39:49 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,3 @@
+console.log("Hi! I am Akshay");
+
+// this is comment 
---
+++ variable.js
@@ -0,0 +1,14 @@
+// Variable used to store some informaion
+// We can use that info later
+// we can also change that information
+
+// variable declaration
+var bucket="Akshay";
+
+// use a variable
+console.log(bucket);
+
+// changing the value of the variable
+bucket="Ashish";
+
+console.log(bucket);   // we can also use var here 
